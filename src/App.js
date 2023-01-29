5737b3594e2efee80f13e2f7b826ad6d3afd5403
pourquoi21 <miyaku219@naver.com>
2023-01-29 21:59:17 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "98.css";
 
 function App() {
+  function MainWindow() {
+    const [count, setCount] = React.useState(0);
+    return (
+      <div style={{ width: 300 }} className="window">
+        <div className="title-bar">
+          <div className="title-bar-text">A wild alert!</div>
+          <div className="title-bar-controls">
+            <button aria-label="Minimize" />
+            <button aria-label="Maximize" />
+            <button aria-label="Close" />
+          </div>
+        </div>
+
+        <div className="window-body">
+          <p style={{ textAlign: "center" }}>입장하시겠습니까?</p>
+          <div className="field-row" style={{ justifyContent: "center" }}>
+            <button onClick={() => setCount(count + 1)}>Yes</button>
+            <button onClick={() => setCount(count - 1)}>네</button>
+            <button onClick={() => setCount(0)} disabled>
+              아니오
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <MainWindow />
     </div>
   );
 }
